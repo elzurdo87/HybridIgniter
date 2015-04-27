893dc453a4f711280d24d1618d0cd290e4600787
elzurdo87 <elzurdo87@gmail.com>
2015-04-27 09:28:43 -0300

    Update config.php

--- application/third_party/config.php
+++ application/third_party/config.php
@@ -17,20 +17,20 @@ return
 			// openid providers			
 			"Google" => array (
 				"enabled" => true,
-				"keys"    => array ( "id" => "630215451844-31p7csb236jco6038mtuds0r6m4aqgun.apps.googleusercontent.com", "secret" => "qTfStxDxFZiXjMuYZGs9F4Pu" ),
+				"keys"    => array ( "id" => "id_", "secret" => "secret" ),
                                 "scope" => "https://www.googleapis.com/auth/userinfo.email" ,
                              //   "access_type"=>"online",
 			),
 
 			"Facebook" => array (
 				"enabled" => true,
-				"keys"    => array ( "id" => "986691278007541", "secret" => "b2d4155d3f023d5000ddbebe4d1de0c0" ),
+				"keys"    => array ( "id" => "id", "secret" => "secret" ),
 				"trustForwarded" => false
 			),
 
 			"Twitter" => array (
 				"enabled" => true,
-				"keys"    => array ( "key" => "U7ZuyFdYTMrkE926s5v8aU7Vx", "secret" => "1dYVlPKs3jOH3ywvlwlDMTYd5F0fk91weQ4M4hV1AS5r1ZR8BT" )
+				"keys"    => array ( "key" => "id", "secret" => "secret" )
 			),
 
 		),
